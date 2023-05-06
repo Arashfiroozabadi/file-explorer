bdeacce56b74d517023b094c161d4b83b90cf19d
firoozabadi <arashfiroozabadii@gmail.com>
2023-05-06 19:28:19 +0330

    done

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,25 @@ import './App.css'
 import FileExplorer from "./features/fileExplorer";
 
 function App() {
-	return (
-		<div className="App">
-			<FileExplorer/>
-		</div>
-	)
+
+
+    // function test(str: string) {
+    //
+    //     const _strLength = str.length;
+    //
+    //     const leftStr = str.slice(0, _strLength / 2)
+    //     const rightStr = str.slice(_strLength / 2)
+    //
+    //     console.log(leftStr, rightStr, rightStr === leftStr);
+    // }
+    //
+    // test('abcabc');
+
+    return (
+        <div className="App">
+            <FileExplorer/>
+        </div>
+    )
 }
 
 export default App
